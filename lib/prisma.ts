b2d8a56eca689ca564e8c970fc7ca8806e900467
ImJopeh <jopedev@gmail.com>
2026-02-02 18:03:17 +0700

    Fix: Resolve TypeScript error in prisma.ts by setting DATABASE_URL environment variable

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,17 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 const databaseUrl = `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+process.env.DATABASE_URL = databaseUrl;
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
-        datasources: {
-            db: {
-                url: databaseUrl,
-            },
-        },
         log: ["query"],
     });
 
